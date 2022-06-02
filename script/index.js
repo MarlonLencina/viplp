8ede685560862455196468f0595a87f03fe98198
marlo <marlo@DESKTOP-D709FCC.localdomain>
2022-06-01 22:54:47 -0300

    mvp

--- script/index.js
+++ script/index.js
@@ -1,4 +1,3 @@
-
 // acordion open/close logic
 
 const acordions = document.getElementsByClassName("accordion__card")
@@ -21,18 +20,67 @@ for (let accordion of acordions) {
     })
 }
 
-// scroll 
+// scroll
 
-const FAQ = document.querySelector("#FAQ")
+const allOptions = document.querySelectorAll(".header__list__item")
+const hstart = document.querySelector("#hstart")
+const hbenefits = document.querySelector("#hbenefits")
+const hhowitworks = document.querySelector("#hhowitworks")
+const hfaq = document.querySelector("#hfaq")
+const hpartners = document.querySelector("#hpartners")
 
+const getPositionY = () => {
+    const scrollY = window.pageYOffset
 
-FAQ.addEventListener("scroll", (e) => console.log(e))
+    allOptions.forEach((item) => {
+        item.classList.remove("active")
+    })
+
+    if (scrollY >= 0 && scrollY < 680) {
+        hstart.classList.add("active")
+    }
+    else if (scrollY >= 680 && scrollY < 2875) {
+        hbenefits.classList.add("active")
+    }
+    else if (scrollY >= 2875 && scrollY < 3533) {
+        hhowitworks.classList.add("active")
+    }
+    else if (scrollY >= 3533 && scrollY < 4135) {
+        hfaq.classList.add("active")
+
+    } else {
+        hpartners.classList.add("active")
+    }
+
+}
+
+window.addEventListener("scroll", getPositionY)
 
 // sidebar on and off
 
 const sidebarButton = document.querySelector("#sidebarButton")
+const headerNavigation = document.querySelector(".header__nav")
 
+const hasAlreadyActive = Array.from(headerNavigation.classList)
+const iconImg = sidebarButton.childNodes[1]
 
 sidebarButton.addEventListener("click", () => {
-    console.log("triste")
+
+    if (hasAlreadyActive.includes("mobile__active")) {
+        headerNavigation.classList.remove("mobile__active")
+        iconImg.src = "./img/assets/sidebaricon.svg"
+    } else {
+        headerNavigation.classList.add("mobile__active")
+        iconImg.src = "./img/assets/closeicon.svg"
+
+    }
+
+
+})
+
+allOptions.forEach((item) => {
+    item.addEventListener("click", () => {
+        headerNavigation.classList.remove("mobile__active")
+        iconImg.src = "./img/assets/sidebaricon.svg"
+    })
 })
